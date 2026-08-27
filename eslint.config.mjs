e8172cc3a13631e4a4f1c48db5e71c904f23f790
Adil Hanney <adilhanney@disroot.org>
2026-08-27 20:10:11 +0100

    chore: enable eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+    files: ['src/**/*.{js,ts}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    rules: {
+        'no-case-declarations': 'off',
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            "args": "all",
+            "argsIgnorePattern": "^_",
+            "caughtErrors": "all",
+            "caughtErrorsIgnorePattern": "^_",
+            "destructuredArrayIgnorePattern": "^_",
+            "varsIgnorePattern": "^_",
+            "ignoreRestSiblings": true
+        }],
+        "prefer-const": ["warn", {
+            "destructuring": "all",
+        }]
+    }
+});
